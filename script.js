1142f59348b9711241a23b0186b2ff2d18ddeff6
AyushChaursiaya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-02-20 17:48:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const parseCode = (str) => {
-    const [firstName, lastName, id] = str.split('0').filter(Boolean);
-    return { firstName, lastName , id };
+    const [firstName, lastName, id] = str.split("0").filter(Boolean);
+	return {firstName, lastName, id};
 };
 
 
 // Do not change the code below
-const str = prompt("Enter str: ");
-alert(JSON.stringify(parseCode(str)));
+// const str = prompt("Enter str: ");
+// alert(JSON.stringify(parseCode(str)));
